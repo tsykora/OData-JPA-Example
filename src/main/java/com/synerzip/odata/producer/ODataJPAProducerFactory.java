169932e3351111261767942b4ac9f7dc74c9b9bf
tsykora <>
2013-03-10 21:46:54 +0100

    Working example with Producer and Consumer. Start producer for filling entries and then start consumer for work with exposed data.

--- src/main/java/com/synerzip/odata/producer/ODataJPAProducerFactory.java
+++ src/main/java/com/synerzip/odata/producer/ODataJPAProducerFactory.java
@@ -5,26 +5,25 @@ import org.odata4j.producer.ODataProducerFactory;
 import org.odata4j.producer.inmemory.InMemoryProducer;
 
 import java.util.Properties;
+import org.core4j.xml.XElement;
+import org.core4j.xml.XNamespace;
+import org.odata4j.producer.jpa.JPAProducer;
 
 public class ODataJPAProducerFactory implements ODataProducerFactory {
 
-	private String persistenceUnitName = "odataJPAService";
-	private String namespace = "odataJPA";
-	private int maxResults = 50;
+    private String persistenceUnitName = "odataJPAService";
+    private String namespace = "odataJPA";
+    private int maxResults = 50;
 
-	@Override
-	public ODataProducer create(Properties arg0) {
+    @Override
+    public ODataProducer create(Properties arg0) {
 //		EntityManagerFactory emf = Persistence
 //				.createEntityManagerFactory(persistenceUnitName);
 
 //		JPAProducer producer = new JPAProducer(emf, namespace, maxResults);
-
-      InMemoryProducer producer = new InMemoryProducer("odata");
-
-
-      // zjistit jak ho nakrmit
-
-
-		return producer;
-	}
+        
+        InMemoryProducer producer = new InMemoryProducer("odata");
+        
+        return producer;
+    }  
 }
